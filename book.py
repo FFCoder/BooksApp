f8714a94fe5b62396fdd395d039766c3578a2ed6
Jonathon <jonathonc.94@gmail.com>
2017-12-20 05:01:00 -0500

    Forgot to Save Book Class

---
+++ book.py
@@ -0,0 +1,13 @@
+class Book(object):
+    def __init__(self,title,author,isbn=None,genre=None,coverimage=None):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.genre = genre
+        self.coverimage = coverimage
+
+    def set_isbn(self,isbn):
+        """Setter Method for isbn
+        TODO: Add Logic to check isbn
+        """
+        self.isbn = isbn
